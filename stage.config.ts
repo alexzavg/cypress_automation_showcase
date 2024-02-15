652dfcff1e3a4060ae1a1567cedc924adca757e1
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2024-02-15 21:53:48 +0200

    multienv setup

---
+++ stage.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+module.exports = defineConfig({
+  projectId: '42mm89',
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--disable-dev-shm-usage');
+          launchOptions.args.push('--disable-gpu');
+          launchOptions.args.push('--no-sandbox');
+
+          return launchOptions;
+        }
+      });
+    },
+    baseUrl: 'https://bing.com'
+  },
+});
