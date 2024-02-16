39713ae1ef40513413642872e46ed2478ea0e7b7
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2024-02-16 21:17:58 +0200

    upd

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,15 @@
+import { DataGenerator } from "../support/DataGenerator";
+
+const data = new DataGenerator();
+const email = await data.randomEmail();
+
 describe('template spec', () => {
   it('passes', () => {
+    
     const envName = Cypress.env('envName');
     cy.log(`Running tests in ${envName} environment`);
+    cy.log(email);
     cy.visit('');
-    cy.wait(3000);
+    cy.wait(15000);
   })
 })
