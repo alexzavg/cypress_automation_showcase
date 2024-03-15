0ddfd237acaab78b2b0c43b0f3cfa94630270042
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2024-03-15 20:39:17 +0200

    test

--- cypress/tests/E2E/table.spec.cy.ts
+++ cypress/tests/E2E/table.spec.cy.ts
@@ -26,16 +26,21 @@ describe('Dynamic Tables', () => {
     });
   });
 
-  it('Dynamic table stats for multiple browsers', () => {
-    const statValuesPromises = Object.values(browsers).map(browserName => {
-      return practicePage.getBrowserStats(browserName, stats.CPU);
-    });
-    
-    return Promise.all(statValuesPromises).then((statValues: string[]) => {
-      Object.keys(browsers).forEach((key, index) => {
-        const browserName = browsers[key as keyof typeof browsers];
-        cy.log(`${browserName} ${stats.CPU}: ${statValues[index]}`);
+  it.only('Dynamic table stats for multiple browsers', () => {
+    const browserNames = Object.values(browsers); // ['Internet Explorer', 'Chrome', 'Firefox']
+    const cpuStats = [];
+
+    // Chain all getBrowserStats calls sequentially
+    browserNames.reduce((chain, browserName) => {
+      return chain.then(() => {
+        return practicePage.getBrowserStats(browserName, stats.CPU).then(statValue => {
+          cpuStats.push(statValue);
+          cy.log(`${browserName} ${stats.CPU}: ${statValue}`);
+        });
       });
+    }, cy.wrap(null)).then(() => {
+      // All CPU stats are fetched, perform any additional checks or operations here.
+      expect(cpuStats).to.have.length(browserNames.length);
     });
   });
 
